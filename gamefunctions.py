b4814e76ba42c16b0447ae216c984465e1485d3d
tuckerm22 <114370670+tuckerm22@users.noreply.github.com>
2025-03-04 17:41:11 -0700

    Update gamefunctions.py

--- gamefunctions.py
+++ gamefunctions.py
@@ -1,10 +1,16 @@
+'''This module contains 4 programs.
+
+    Each program is executed three times with various random inputs
+'''
 #gamefunctions.py
 #Mercedes Tucker
-#02/11/25
+#03/4/25
 
-#This code creates four functions, purchase_item, new_random_monster, print_welcome and print_shop_menu
+#This code creates four functions, purchase_item, new_random_monster,
+#print_welcome and print_shop_menu
 #and calls them 3 times with 3 different inputs
 # first defining and running purchase_item
+
 import random
 
 def purchase_item(itemPrice, startingMoney, quantityToPurchase):
@@ -14,26 +20,17 @@ def purchase_item(itemPrice, startingMoney, quantityToPurchase):
     money_leftover = startingMoney - (quantityToPurchase * itemPrice)
     if money_leftover < 0:
         quantityToPurchase = 0 # Not enough money, return nothing
-        print(quantityToPurchase)
-        return startingMoney
+       
+        return quantityToPurchase, startingMoney
     else:
-        print(quantityToPurchase)
-        return money_leftover  # Enough money, return the remaining amount
-leftovermoney = purchase_item(round((random.random()*10),3),round((random.random()*10),3),
-                              round((random.randint(1, 10)),3))
-leftovermoney2 = purchase_item(round((random.random()*10),3),round((random.random()*10),3),
-                              round((random.randint(1, 10)),3))
-leftovermoney3 = purchase_item(round((random.random()*10),3),round((random.random()*10),3),
-                              round((random.randint(1, 10)),3))
-print(leftovermoney)
-print(leftovermoney2)
-print(leftovermoney3)
+        return quantityToPurchase, money_leftover # Enough money, return the remaining amount
 
 #Now defining and running new_random_monster
 import random
 
 def new_random_monster():
-    """"outputs a random monster based on random inputs from a given set of values
+    """"outputs a random monster based on random inputs from a given
+    set of values
     returns: random monster with description, health, power and money"""
     monster_names = ["Goblin", "Dragon", "Vampire"]
     name = random.choice(monster_names)
@@ -58,13 +55,6 @@ def new_random_monster():
     }
     
     return monster
-my_monster = new_random_monster()
-my_monster2 = new_random_monster()
-my_monster3 = new_random_monster()
-print(my_monster)
-print(my_monster2)
-print(my_monster3)
-
 
 # Now defining and running print_welcome
 def print_welcome(name, width = 20):
@@ -74,10 +64,6 @@ def print_welcome(name, width = 20):
     phrase = f' Hello, {name}!'
     phrase_aligned = f'{phrase:^{width}}'
     print(phrase_aligned)
-print_welcome("John")
-print_welcome("Mercedes")
-print_welcome("Riley")
-
 # Lastly defining and running print_shop_menu
 def print_shop_menu(item1Name, item1Price, item2Name, item2Price):
     """outputs a shop menu given 2 items and their prices
@@ -93,6 +79,35 @@ def print_shop_menu(item1Name, item1Price, item2Name, item2Price):
     print(first_item_centered)
     print(second_item_centered)
     print("\\----------------------/")
-print_shop_menu("Egg", .23, "Pear", 12.34)
-print_shop_menu("Bread", 5, "Apple", 1)
-print_shop_menu("Milk", 2, "Berries", 6)
+def test_functions():
+    leftovermoney = purchase_item(round((random.random() * 10), 3),
+                                  round((random.random() * 10), 3),
+                                  round((random.randint(1, 10)), 3))
+    leftovermoney2 = purchase_item(round((random.random() * 10), 3),
+                                   round((random.random() * 10), 3),
+                                   round((random.randint(1, 10)), 3))
+    leftovermoney3 = purchase_item(round((random.random() * 10), 3),
+                                   round((random.random() * 10), 3),
+                                   round((random.randint(1, 10)), 3))
+    print(leftovermoney)
+    print(leftovermoney2)
+    print(leftovermoney3)
+
+    my_monster = new_random_monster()
+    my_monster2 = new_random_monster()
+    my_monster3 = new_random_monster()
+    print(my_monster)
+    print(my_monster2)
+    print(my_monster3)
+
+    print_welcome("John")
+    print_welcome("Mercedes")
+    print_welcome("Riley")
+
+    print_shop_menu("Egg", .23, "Pear", 12.34)
+    print_shop_menu("Bread", 5, "Apple", 1)
+    print_shop_menu("Milk", 2, "Berries", 6)
+
+if __name__ == "__main__":
+    test_functions()
+
